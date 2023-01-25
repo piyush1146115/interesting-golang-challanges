1119e2b1e273dc7e3873134f0113aa9d7ab3bba9
piyush1146115 <piyush@appscode.com>
2023-01-25 14:33:42 +0600

    Read concurrency.

---
+++ Golang Tour/channel_example_range_fibanocci.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Ref: https://go.dev/tour/concurrency/4
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+func main() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
